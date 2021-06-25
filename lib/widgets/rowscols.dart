7a03854bb7272165c5ecb7e8263e256a62abcc3c
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2021-06-25 12:14:27 +0530

    Update rowscols.dart

--- lib/widgets/rowscols.dart
+++ lib/widgets/rowscols.dart
@@ -1,80 +1,52 @@
 import 'package:flutter/material.dart';
 
-class RowsCols extends StatefulWidget {
+class RowsCols extends StatelessWidget {
   const RowsCols({Key? key}) : super(key: key);
 
   @override
-  _RowsColsState createState() => _RowsColsState();
-}
-
-class _RowsColsState extends State<RowsCols> {
-  @override
   Widget build(BuildContext context) {
-    var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
+    var h = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(title: Text('Rows and Columns')),
       body: Container(
-          color: Colors.yellow,
-          height: 150,
+          height: 200,
           width: w,
-          child: Wrap(
-            direction: Axis.horizontal,
-            alignment: WrapAlignment.center,
-            children: [
-              Container(height: 30, width: 30, color: Colors.red),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Text('hello'),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Container(height: 30, width: 30, color: Colors.red),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Text('hello'),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Container(height: 30, width: 30, color: Colors.red),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Text('hello'),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Container(height: 30, width: 30, color: Colors.red),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Text('hello'),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Container(height: 30, width: 30, color: Colors.red),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-              Text('hello'),
-              Container(height: 30, width: 30, color: Colors.blue),
-              Container(height: 30, width: 30, color: Colors.green),
-            ],
-          )
-          // Column(
+          color: Colors.yellow,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(height: 60, width: 60, color: Colors.red),
+                Container(height: 60, width: 60, color: Colors.blue),
+                Container(height: 60, width: 60, color: Colors.green),
+                Container(height: 60, width: 60, color: Colors.black),
+                Container(height: 60, width: 60, color: Colors.purple),
+              ])
+          //  Wrap(
+          //   direction: Axis.vertical,
+          //   alignment: WrapAlignment.start,
+          //   // mainAxisAlignment: MainAxisAlignment.start,
+          //   // crossAxisAlignment: CrossAxisAlignment.center,
           //   children: [
-          //     Container(height: 30, width: 30, color: Colors.red),
-          //     Container(height: 30, width: 30, color: Colors.blue),
-          //     Container(height: 30, width: 30, color: Colors.green),
-          //     Text('hello'),
-          //     Container(height: 30, width: 30, color: Colors.blue),
-          //     Container(height: 30, width: 30, color: Colors.green),
+          // Container(height: 60, width: 60, color: Colors.red),
+          // Container(height: 60, width: 60, color: Colors.blue),
+          // Container(height: 60, width: 60, color: Colors.green),
+          // Container(height: 60, width: 60, color: Colors.black),
+          // Container(height: 60, width: 60, color: Colors.purple),
+          //   ],
+          // ),
+          //     Row(
+          //   mainAxisAlignment: MainAxisAlignment.start,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          //     Container(height: 60, width: 60, color: Colors.red),
+          //     Container(height: 60, width: 60, color: Colors.blue),
+          //     Container(height: 60, width: 60, color: Colors.green),
+          //     Container(height: 60, width: 60, color: Colors.black),
+          //     Container(height: 60, width: 60, color: Colors.purple),
           //   ],
           // )
-          // Row(
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: [
-          // Container(height: 30, width: 30, color: Colors.red),
-          // Container(height: 30, width: 30, color: Colors.blue),
-          // Container(height: 30, width: 30, color: Colors.green),
-          // Text('hello'),
-          // Container(height: 30, width: 30, color: Colors.blue),
-          // Container(height: 30, width: 30, color: Colors.green),
-          //     ])
           ),
     );
   }
