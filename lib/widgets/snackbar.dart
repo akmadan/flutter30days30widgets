dd107728d5e657386fb4563c71f8efe80d001bbe
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2021-06-29 13:00:03 +0530

    Update snackbar.dart

--- lib/widgets/snackbar.dart
+++ lib/widgets/snackbar.dart
@@ -1,39 +1,35 @@
 import 'package:flutter/material.dart';
 
-class SnackBarWidget extends StatelessWidget {
-  const SnackBarWidget({Key? key}) : super(key: key);
+class SnackbarWidget extends StatelessWidget {
+  const SnackbarWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            final snackBar = SnackBar(
-              backgroundColor: Colors.grey.shade900,
-              content: const Text(
-                'This is a snackBar',
-                style: TextStyle(color: Colors.white),
-              ),
-              duration: const Duration(milliseconds: 1500),
-              // width: 280.0,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 8.0,
-              ),
-              behavior: SnackBarBehavior.floating,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              action: SnackBarAction(
-                label: 'Undo',
-                textColor: Colors.white,
-                onPressed: () {},
-              ),
-            );
-
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
-          child: Text('Show SnackBar'),
+      appBar: AppBar(
+        // backgroundColor: Colors.black,
+        title: Text('Snackbar'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                final snackbar = SnackBar(
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      textColor: Colors.blue,
+                      onPressed: () {},
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    behavior: SnackBarBehavior.floating,
+                    // padding: EdgeInsets.all(20),
+                    duration: const Duration(milliseconds: 3000),
+                    // backgroundColor: Colors.red,
+                    content: Text('This is an error'));
+                ScaffoldMessenger.of(context).showSnackBar(snackbar);
+              },
+              child: Text('Show Snackbar')),
         ),
       ),
     );
