6051f046ed3be0ca852c15d7a3bdd1852a79c833
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2021-07-05 10:52:45 +0530

    Update animatedtext.dart

--- lib/widgets/animatedtext.dart
+++ lib/widgets/animatedtext.dart
@@ -1,78 +1,67 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
-class AnimatedTextWidget extends StatefulWidget {
+class AnimatedTextWidget extends StatelessWidget {
   const AnimatedTextWidget({Key? key}) : super(key: key);
 
   @override
-  _AnimatedTextWidgetState createState() => _AnimatedTextWidgetState();
-}
-
-class _AnimatedTextWidgetState extends State<AnimatedTextWidget> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Animated Text')),
+      appBar: AppBar(
+        title: Text('AnimatedText'),
+      ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          AnimatedTextKit(
-            animatedTexts: [
-              TypewriterAnimatedText(
-                'Akshit Madan',
-                textStyle: const TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                speed: const Duration(milliseconds: 100),
-              ),
-            ],
-            totalRepeatCount: 4,
-            pause: const Duration(milliseconds: 200),
-            displayFullTextOnTap: true,
-            stopPauseOnTap: true,
-          ),
-          AnimatedTextKit(
-            animatedTexts: [
-              RotateAnimatedText(
-                'HELLO',
-                textStyle: const TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              RotateAnimatedText(
-                'BEAUTIFUL',
-                textStyle: const TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              RotateAnimatedText(
-                'WORLD',
-                textStyle: const TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-            onTap: () {
-              print("Tap Event");
-            },
-          ),
-          AnimatedTextKit(
-            animatedTexts: [
-              WavyAnimatedText('Hello World'),
-              WavyAnimatedText('Look at the waves'),
-            ],
-            isRepeatingAnimation: true,
-            onTap: () {
-              print("Tap Event");
-            },
-          ),
-        ],
-      )),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // AnimatedTextKit(
+          //   animatedTexts: [
+          //     TypewriterAnimatedText('Akshit Madan',
+          //         textStyle:
+          //             TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          //         speed: Duration(milliseconds: 400))
+          //   ],
+          //   totalRepeatCount: 4,
+          //   pause: Duration(milliseconds: 200),
+          //   displayFullTextOnTap: true,
+          //   stopPauseOnTap: true,
+          // ),
+          // AnimatedTextKit(
+          //   animatedTexts: [
+          //     RotateAnimatedText(
+          //       'HELLO',
+          //       textStyle: TextStyle(
+          //           fontSize: 30,
+          //           fontWeight: FontWeight.w600,
+          //           color: Colors.red),
+          //     ),
+          //     RotateAnimatedText(
+          //       'BEAUTIFUL',
+          //       textStyle: TextStyle(
+          //           fontSize: 30,
+          //           fontWeight: FontWeight.w600,
+          //           color: Colors.red),
+          //     ),
+          //     RotateAnimatedText(
+          //       'WORLD',
+          //       textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          //     ),
+          //   ],
+          //   // totalRepeatCount: 4,
+          //   // pause: Duration(milliseconds: 200),
+          //   // displayFullTextOnTap: true,
+          //   // stopPauseOnTap: true,
+          // ),
+          AnimatedTextKit(animatedTexts: [
+            WavyAnimatedText(
+              'HELLO WORLD',
+              textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            WavyAnimatedText(
+              'HELLO WORLD',
+              textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ])
+        ]),
+      ),
     );
   }
 }
